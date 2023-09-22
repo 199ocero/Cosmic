ae291ceace18163aa82761191e7b3bd0ebc89bc8
199ocero <199ocero@gmail.com>
2023-09-22 16:11:21 +0800

    remove default key

--- app/Models/ColorCombination.php
+++ app/Models/ColorCombination.php
@@ -12,7 +12,6 @@ class ColorCombination extends Model
     protected $fillable = [
         'name',
         'text_color',
-        'bg_color',
-        'default',
+        'bg_color'
     ];
 }
--- database/migrations/2023_09_21_043400_create_color_combinations_table.php
+++ database/migrations/2023_09_21_043400_create_color_combinations_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('name');
             $table->string('text_color');
             $table->string('bg_color');
-            $table->boolean('default')->default(true);
             $table->timestamps();
         });
     }
