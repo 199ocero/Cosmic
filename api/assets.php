0202daf2db83fc775e6644bae28f79352326f95f
199ocero <199ocero@gmail.com>
2023-09-23 10:46:46 +0800

    added vercel fix

--- api/assets.php
+++ api/assets.php
@@ -6,8 +6,10 @@
  */
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    readfile(__DIR__ . '/../public/css/' . basename($_GET['file']));
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    readfile(__DIR__ . '/../public/js/' . basename($_GET['file']));
+} else {
+    http_response_code(404); // Handle other file types or invalid requests here
 }
