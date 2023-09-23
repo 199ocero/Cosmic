cfdfb800f809d5ff07066251476c5fc1cea4fbfb
199ocero <199ocero@gmail.com>
2023-09-23 10:51:14 +0800

    added vercel fix

--- api/assets.php
+++ api/assets.php
@@ -6,10 +6,10 @@
  */
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    readfile(__DIR__ . '/../public/css/' . basename($_GET['file']));
+    readfile(__DIR__ . '/../public/css/filament/' . basename($_GET['file']));
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    readfile(__DIR__ . '/../public/js/' . basename($_GET['file']));
+    readfile(__DIR__ . '/../public/js/filament/' . basename($_GET['file']));
 } else {
     http_response_code(404); // Handle other file types or invalid requests here
 }
