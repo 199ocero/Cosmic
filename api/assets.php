58f064b0fe5af670f0b6f5d1976baee90b6820c5
199ocero <199ocero@gmail.com>
2023-09-23 10:03:52 +0800

    fix assets.php

--- api/assets.php
+++ api/assets.php
@@ -7,8 +7,24 @@
  */
 if ($_GET['type'] === 'css') {
     header("Content-type: text/css; charset: UTF-8");
-    echo require __DIR__ . '/../public/css/' . basename($_GET['file']);
+    $cssFile = __DIR__ . '/../public/css/' . basename($_GET['file']);
+
+    if (file_exists($cssFile)) {
+        readfile($cssFile);
+    } else {
+        // Handle the case where the CSS file does not exist
+        // For example, you could output a default CSS or send a 404 error.
+        // Example: header("HTTP/1.0 404 Not Found");
+    }
 } else if ($_GET['type'] === 'js') {
     header('Content-Type: application/javascript; charset: UTF-8');
-    echo require __DIR__ . '/../public/js/' . basename($_GET['file']);
+    $jsFile = __DIR__ . '/../public/js/' . basename($_GET['file']);
+
+    if (file_exists($jsFile)) {
+        readfile($jsFile);
+    } else {
+        // Handle the case where the JS file does not exist
+        // For example, you could output a default JS or send a 404 error.
+        // Example: header("HTTP/1.0 404 Not Found");
+    }
 }
