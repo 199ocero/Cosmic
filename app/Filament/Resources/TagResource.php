ef80d88007cb02824a5ce7eee82834b3559c627d
199ocero <199ocero@gmail.com>
2023-09-24 13:11:21 +0800

    fix navigation sort

--- app/Filament/Resources/TagResource.php
+++ app/Filament/Resources/TagResource.php
@@ -25,7 +25,7 @@ class TagResource extends Resource
 
     protected static ?string $navigationLabel = 'Tags';
 
-    protected static ?int $navigationSort = 2;
+    protected static ?int $navigationSort = 3;
 
     public static function form(Form $form): Form
     {
