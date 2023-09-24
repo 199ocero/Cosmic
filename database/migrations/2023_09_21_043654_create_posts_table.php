1b96265be4e1222cff9add3702a9040af4927105
199ocero <199ocero@gmail.com>
2023-09-24 12:03:47 +0800

    fix migration

--- database/migrations/2023_09_21_043654_create_posts_table.php
+++ database/migrations/2023_09_21_043654_create_posts_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(App\Models\User::class);
+            $table->foreignId('user_id')->nullable()->references('id')->on('users')->onDelete('set null');
             $table->string('thumbnail')->nullable();
             $table->string('title');
             $table->string('slug')->unique();
